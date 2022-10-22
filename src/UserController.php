d8405c639195fc62aa030cff7729880b5067085d
Alban-Ktz <alban.ktz@gmail.com>
2022-10-22 09:19:36 +0200

    usercontroller

--- src/UserController.php
+++ src/UserController.php
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-<?php
-namespace App;
-
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Slim\Views\Twig;
-
-class UserController
-{
-  private $view;
-
-  public function __construct(Twig $view, UserService $userService)
-  {
-    $this->view = $view;
-    $this->userService = $userService;
-  }
-
-  public function test(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
-  {
-    $user = $this->userService->signUp('test');
-    return $this->view->render($response, 'hello.twig', [
-      'name' => 'me',
-    ]);
-    return $response;
-  }
-}
-=======
 <?php
 namespace App;
 
@@ -53,4 +25,3 @@ class UserController
     return $response;
   }
 }
->>>>>>> 51e56ca710879ad2747d9c72024b4f1316db2000
